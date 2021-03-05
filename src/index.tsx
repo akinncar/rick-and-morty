e4d28be0691e0e684906c9aad3ed99834551806e
akinncar <akinncar@hotmail.com>
2021-03-04 21:08:10 -0300

    Install ios and add logo

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import Routes from './routes';
 import { ApolloProvider } from '@apollo/client';
 import client from './services/client';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
+      <StatusBar barStyle="light-content" />
       <Routes />
     </ApolloProvider>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect } from 'react';
-import { View, Text, ActivityIndicator, Image, Dimensions } from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  ActivityIndicator,
+  Image,
+  Dimensions,
+} from 'react-native';
 import { gql, useQuery } from '@apollo/client';
 import { FlatList } from 'react-native-gesture-handler';
 import Animated, {
@@ -88,14 +95,33 @@ export default function Home() {
         backgroundColor: '#111',
       }}
     >
-      <FlatList
-        style={{
-          width: Dimensions.get('window').width,
-        }}
-        keyExtractor={(item) => item.id}
-        data={data.characters.results}
-        renderItem={({ item }: any) => <HomeItem item={item} />}
-      />
+      <SafeAreaView>
+        <View
+          style={{
+            display: 'flex',
+            width: Dimensions.get('window').width,
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            style={{
+              display: 'flex',
+              width: Dimensions.get('window').width * 0.7,
+              height: 120,
+              resizeMode: 'contain',
+            }}
+            source={require('../../assets/images/logo.png')}
+          />
+        </View>
+        <FlatList
+          style={{
+            width: Dimensions.get('window').width,
+          }}
+          keyExtractor={(item) => item.id}
+          data={data.characters.results}
+          renderItem={({ item }: any) => <HomeItem item={item} />}
+        />
+      </SafeAreaView>
     </View>
   );
 }
